aaccfed0233b1e02558ae522de58318277c5251d
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-11-05 10:57:50 +0000

    Final App

--- index.jsx
+++ index.jsx
@@ -4,5 +4,15 @@ import React from "react";
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-    <img src="react-logo.svg" />
+    <main>
+        <img src="react-logo.svg" />
+        <h1>Fun Facts About React!!!</h1>
+        <ul>
+            <li>First Release in 2013</li>
+            <li>Was Originally created by Jordan Walke</li>
+            <li>It's maintainded by META</li>
+            <li>Powers thousands of Enterprise Apps including mobile Apps</li>
+        </ul>
+    </main>
+    
 )
